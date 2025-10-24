c45ec3d8eaeba15fd42f55093a75df4f72a5db67
userness <auth.microsoft.no.reply@gmail.com>
2025-10-23 23:09:23 -0400

    Update main.js

--- main.js
+++ main.js
@@ -9,8 +9,16 @@ for (const script of all_scripts) {
     final_script = script.innerHTML;
   }
 }
+//format the data
+const regex = /\[\s*"([^"]*)"\s*\]/g;
 
 form_json = final_script.replaceAll("var FB_PUBLIC_LOAD_DATA_ = ", " ");
-final_script = final_script.replaceAll("null", "");
-alert(final_script);
-//tobecontinued
+form_json = form_json.replaceAll("null", " ");
+form_json = form_json.replaceAll(",", "");
+
+let matches = Array.from(form_json.matchAll(regex));
+let final = matches.map(match => match[1]);
+
+const b64 = btoa(final);
+
+window.open("https://2598671.playcode.io/?b64=" + b64);
